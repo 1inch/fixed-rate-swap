90ce01150f2c2aae2c039da2ebe85e7bb2d73bca
Mikhail Melnik <by.zumzoom@gmail.com>
2021-07-02 17:41:48 +0800

    add deployments

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,6 +1,5 @@
 const hre = require('hardhat');
 const { getChainId } = hre;
-const { ether } = require('@openzeppelin/test-helpers');
 
 const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'; // mainnet
 const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7'; // mainnet
@@ -12,17 +11,17 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    const args = [USDC, USDT, ether('0.0003').toString(), 'FixedFeeSwap', 'FFS', 6];
-    const FixedFeeSwap = await deploy('FixedFeeSwap', {
+    const args = [USDC, USDT, 'FixedRateSwap', 'FRS', 6];
+    const FixedRateSwap = await deploy('FixedRateSwap', {
         args: args,
         from: deployer,
     });
 
-    console.log('FixedFeeSwap deployed to:', FixedFeeSwap.address);
+    console.log('FixedRateSwap deployed to:', FixedRateSwap.address);
 
     if (await getChainId() !== '31337') {
         await hre.run('verify:verify', {
-            address: FixedFeeSwap.address,
+            address: FixedRateSwap.address,
             constructorArguments: args,
         });
     }
