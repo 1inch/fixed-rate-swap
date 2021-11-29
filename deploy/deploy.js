52552c22f88ba3e628479bcb34bca541e9812ab6
Mikhail Melnik <by.zumzoom@gmail.com>
2021-11-29 14:24:46 +0800

    add other deployments

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,8 +1,26 @@
 const hre = require('hardhat');
 const { getChainId } = hre;
 
-const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'; // mainnet
-const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7'; // mainnet
+// mainnet
+const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+const DECIMALS = 6;
+// bsc
+// const USDC = '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d';
+// const USDT = '0x55d398326f99059fF775485246999027B3197955';
+// const DECIMALS = 18;
+// matic
+// const USDC = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174';
+// const USDT = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F';
+// const DECIMALS = 6;
+// arbitrum
+// const USDC = '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8';
+// const USDT = '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9';
+// const DECIMALS = 6;
+// optimistic
+// const USDC = '0x7F5c764cBc14f9669B88837ca1490cCa17c31607';
+// const USDT = '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58';
+// const DECIMALS = 6;
 
 module.exports = async ({ deployments, getNamedAccounts }) => {
     console.log('running deploy script');
@@ -11,7 +29,7 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    const args = [USDC, USDT, 'FixedRateSwap', 'FRS', 6];
+    const args = [USDC, USDT, 'FixedRateSwap', 'FRS', DECIMALS];
     const FixedRateSwap = await deploy('FixedRateSwap', {
         args: args,
         from: deployer,
--- hardhat.networks.js
+++ hardhat.networks.js
@@ -4,7 +4,6 @@ if (process.env.MAINNET_RPC_URL && process.env.MAINNET_PRIVATE_KEY) {
     networks.mainnet = {
         url: process.env.MAINNET_RPC_URL,
         chainId: 1,
-        gasPrice: 25000000000,
         accounts: [process.env.MAINNET_PRIVATE_KEY],
     };
 }
@@ -13,17 +12,7 @@ if (process.env.BSC_RPC_URL && process.env.BSC_PRIVATE_KEY) {
     networks.bsc = {
         url: process.env.BSC_RPC_URL,
         chainId: 56,
-        gasPrice: 10000000000,
         accounts: [process.env.BSC_PRIVATE_KEY],
-    };
-}
-
-if (process.env.KOVAN_RPC_URL && process.env.KOVAN_PRIVATE_KEY) {
-    networks.kovan = {
-        url: process.env.KOVAN_RPC_URL,
-        chainId: 42,
-        gasPrice: 1000000000,
-        accounts: [process.env.KOVAN_PRIVATE_KEY],
     };
 }
 
@@ -31,7 +20,6 @@ if (process.env.MATIC_RPC_URL && process.env.MATIC_PRIVATE_KEY) {
     networks.matic = {
         url: process.env.MATIC_RPC_URL,
         chainId: 137,
-        gasPrice: 1000000000,
         accounts: [process.env.MATIC_PRIVATE_KEY],
     };
 }
@@ -40,8 +28,15 @@ if (process.env.ARBITRUM_RPC_URL && process.env.ARBITRUM_PRIVATE_KEY) {
     networks.arbitrum = {
         url: process.env.ARBITRUM_RPC_URL,
         chainId: 42161,
-        gasPrice: 250000000,
         accounts: [process.env.ARBITRUM_PRIVATE_KEY],
+    };
+}
+
+if (process.env.OPTIMISTIC_RPC_URL && process.env.OPTIMISTIC_PRIVATE_KEY) {
+    networks.optimistic = {
+        url: process.env.OPTIMISTIC_RPC_URL,
+        chainId: 10,
+        accounts: [process.env.OPTIMISTIC_PRIVATE_KEY],
     };
 }
 
